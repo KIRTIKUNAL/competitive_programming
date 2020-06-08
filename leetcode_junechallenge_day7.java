c43670372357173a616a6c3358cbf954bf65d102
KIRTI KUNAL <kirtikunal4@gmail.com>
2020-06-08 10:47:11 +0530

    Create leetcode_junechallenge_day7.java

---
+++ leetcode_junechallenge_day7.java
@@ -0,0 +1,33 @@
+// Coin Change 2
+
+/*
+You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+
+ 
+
+Example 1:
+
+Input: amount = 5, coins = [1, 2, 5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+
+*/
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= coins.length; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i-1][j] + (j >= coins[i-1] ? dp[i][j-coins[i-1]] : 0);
+            }
+        }
+        return dp[coins.length][amount]; 
+    }
+}
